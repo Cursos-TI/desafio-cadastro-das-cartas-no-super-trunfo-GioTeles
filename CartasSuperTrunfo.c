9353ebdbbe6561dd2602e368b03b2298db24cdb3
GioTeles <164383496+GioTeles@users.noreply.github.com>
2025-05-30 17:17:34 +0000

    beta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,20 +6,20 @@ int main() {
     /* Declaração das variáveis presentes nas cartas. Nas linhas 10, 11 e 12 há o uso dos números '1' e '2' para diferenciar as variáveis
     pertencentes as cartas 1 e 2, respectivamente. */
 
-    char Estado_1, Estado_2, Codigo_Da_Carta_1[4], Codigo_Da_Carta_2[4], Cidade_1[30], Cidade_2[30];
+    char Estado_1, Estado_2, Codigo_Da_Carta_1[4], Codigo_Da_Carta_2[4], Cidade_1[50], Cidade_2[50];
     int Populacao_1, Populacao_2, Pontos_Turisticos_1, Pontos_Turisticos_2;
-    float Area_1, Area_2, PIB_1, PIB_2;
+    float Area_1, Area_2, PIB_1, PIB_2, Densidade_Populacional_1, Densidade_Populacional_2, PIB_per_Capita_1, PIB_per_Capita_2;
 
     // Cadastrando os dados da carta 1
 
-    printf("Digite a letra que representa o Estado da carta: \n");
+    printf("\nDigite a letra que representa o Estado da carta: \n");
     scanf(" %c", &Estado_1);
 
     printf("Digite o código da carta: (Máximo de 3 caracteres) \n");
     scanf("%s", Codigo_Da_Carta_1);
 
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", Cidade_1);
+    printf("Digite o nome da cidade: --> (Sem espaços) \n");
+    scanf("%s", Cidade_1); // Optei por não usar fgets, então é necessário escrever o nome sem espaços
 
     printf("Digite a população da cidade: \n");
     scanf("%d", &Populacao_1);
@@ -33,6 +33,10 @@ int main() {
     printf("Digite o PIB da cidade: \n");
     scanf("%f", &PIB_1);
 
+    Densidade_Populacional_1 = (float) Populacao_1 / Area_1; // Calculando a densidade populacional da cidade 1
+
+    PIB_per_Capita_1 = (float) PIB_1 / Populacao_1; // Calculando o PIB per Capita da cidade 1
+
     // Cadastrando os dados da carta 2
 
     printf("\nDigite a letra que representa o Estado da carta: \n");
@@ -41,8 +45,8 @@ int main() {
     printf("Digite o código da carta: (Máximo de 3 caracteres) \n");
     scanf("%s", Codigo_Da_Carta_2);
 
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", Cidade_2);
+    printf("Digite o nome da cidade: --> (Sem espaços) \n");
+    scanf("%s", Cidade_2); // Optei por não usar fgets, então é necessário escrever o nome sem espaços
 
     printf("Digite a população da cidade: \n");
     scanf("%d", &Populacao_2);
@@ -56,27 +60,35 @@ int main() {
     printf("Digite o PIB da cidade: \n");
     scanf("%f", &PIB_2);
 
+    Densidade_Populacional_2 = (float) Populacao_2 / Area_2; // Calculando a densidade populacional da cidade 2
+
+    PIB_per_Capita_2 = (float) PIB_2 / Populacao_2; // Calculando o PIB per Capita da cidade 2
+
     // Exibindo as informações cadastradas da carta 1
 
     printf("\nCarta 1: \n");
     printf("Estado: %c\n", Estado_1);
     printf("Código: %s\n", Codigo_Da_Carta_1);
-    printf("Cidade: %s\n", Cidade_1);
-    printf("População: %d de habitantes\n", Populacao_1);
-    printf("Número de Pontos Turisticos: %d\n", Pontos_Turisticos_1);
+    printf("Nome da Cidade: %s\n", Cidade_1);
+    printf("População: %d \n", Populacao_1);
     printf("Área: %f km²\n", Area_1);
     printf("PIB: %f de reais\n", PIB_1);
+    printf("Número de Pontos Turísticos: %d\n", Pontos_Turisticos_1);
+    printf("Densidade Populacional: %.2f hab/km²\n", Densidade_Populacional_1);
+    printf("PIB per Capita: %.2f reais\n", PIB_per_Capita_1);
 
     // Exibindo as informações cadastradas da carta 2
 
     printf("\nCarta 2: \n");
     printf("Estado: %c\n", Estado_2);
     printf("Código: %s\n", Codigo_Da_Carta_2);
-    printf("Cidade: %s\n", Cidade_2);
-    printf("População: %d de habitantes\n", Populacao_2);
-    printf("Número de Pontos Turísticos: %d\n", Pontos_Turisticos_2);
+    printf("Nome da Cidade: %s\n", Cidade_2);
+    printf("População: %d \n", Populacao_2);
     printf("Área: %f km²\n", Area_2);
     printf("PIB: %f de reais\n", PIB_2);
+    printf("Número de Pontos Turísticos: %d\n", Pontos_Turisticos_2);
+    printf("Densidade Populacional: %.2f hab/km²\n", Densidade_Populacional_2);
+    printf("PIB per Capita: %.2f reais\n", PIB_per_Capita_2);
 
     return 0;
 }
